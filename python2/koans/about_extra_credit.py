103197f39d1e73b0c4afccd9bab0db194540fbb2
yan.haifeng <yanheven@qq.com>
2015-09-10 10:27:55 +0800

    910 update

--- python2/koans/about_extra_credit.py
+++ python2/koans/about_extra_credit.py
@@ -9,9 +9,44 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+import itertools
+import random
 
 from runner.koan import *
 
+
+class DiceSet(object):
+    def __init__(self):
+        self._values = None
+
+    @property
+    def values(self):
+        return self._values
+
+    def roll(self, n):
+        # Needs implementing!
+        # Tip: random.randint(min, max) can be used to generate random numbers
+        self._values = [random.randint(1,6) for i in range(n)]
+
+def score(dice):
+    # You need to write this method
+    dice = sorted(dice)
+    point = 0
+    dice_group = itertools.groupby(dice)
+    for k, v in dice_group:
+        length = len(list(v))
+        if length>2:
+            if k == 1:
+                point += 1000 + 100 * (length -3)
+            else:
+                point += k*100
+            if k == 5:
+                point += 50 * (length-3)
+        elif k == 1:
+            point += 100 * length
+        elif k == 5:
+            point += 50 * length
+    return point
 
 class AboutExtraCredit(Koan):
     # Write tests here. If you need extra test classes add them to the
